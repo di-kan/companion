181704db906b54a7b6fbfb789de4fdac40b06288
Dimitris <zerone.dt@gmail.com>
2023-06-23 18:22:50 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,183 @@
+# import pandas as pd
+from flask import Flask, redirect, url_for, request, render_template, flash, session, g
+from flask_session import Session
+import os.path
+from werkzeug.utils import secure_filename
+import utils
+import sqlite3
+
+#-------------------------------
+#INIT SOME VARIABLES
+#--------------------------------
+tags_filename = "tags.json"
+words_filename = "words.json"
+upload_folder = './uploads'
+db_file = "gervoc.db"
+allowed_extensions = {'csv', 'txt'}
+
+
+#--------------------------------
+#NON-FLASK/APP FUNCTIONS
+#--------------------------------
+# def what_tag(ind):
+
+
+
+
+# def assign_tags(word_tags):
+#     global words
+#     #Iterrate all dictionarires in the list
+#     for word in word_tags:
+#         word_ind = list(word.keys())
+#         tags_list = word.values()
+#         words.iloc[word_ind]=str(tags_list)
+#     # save_words(words_filename)
+
+
+def load_tags(filename):
+    if os.path.exists(filename):
+        tmp_tags = pd.read_json(filename)
+    else:
+        tmp = {'name': ['general', 'normal', 'non-normal', 'modal', 'general', 'female', 'male', 'neutral'],
+               'category': ['verb', 'verb', 'verb', 'verb', 'noun', 'noun', 'noun', 'noun']}
+        tmp_tags = pd.DataFrame(tmp)
+        tmp_tags.to_json(filename)
+    return tmp_tags
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+def open_db():
+    conn = getattr(g,'_database', None)
+    if conn is None:
+        conn = g._database = sqlite3.connect(db_file)
+        conn.enable_load_extension(True)
+        conn.load_extension('./venv/lib/python3.11/site-packages/sqlite_regex/regex0.so')
+    return conn
+
+
+#--------------------------------
+#APP FUNCTIONS FLASK START HERE
+#--------------------------------
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = upload_folder
+app.debug = True
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def show_index():
+    global conn
+    conn = open_db()
+    utils.initialize_database(conn)
+    # utils.add_demo_content(conn)
+    return render_template("index.html")
+
+
+@app.route('/words')
+def show_words():
+    global conn
+    conn = open_db()
+    words = utils.get_words_from_db(conn)
+    return render_template("words.html", df=words)
+
+
+@app.route('/import', methods=['POST', 'GET'])
+def import_words():
+    global conn
+    conn = open_db()
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            new_words = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename), delimiter=":", header=None)
+            new_words.rename(columns={0: 'page', 1:'word', 2: 'translation'}, inplace=True)
+            for word in new_words.iterrows():
+                a_word=word[1]
+                the_word = {'word':a_word['word'],
+                            'translation':a_word['translation'],
+                            'from':'german',
+                            'to':'greek'}
+                utils.insert_word(conn, the_word)
+                the_tag = {'page':a_word['page']}
+                utils.tag_word_by_dict(conn, the_word, the_tag)
+            conn.commit()
+            return redirect("/words")
+    utils.delete_all_data(conn)
+    return render_template("import.html")
+
+@app.route('/tags', methods=['GET', 'POST'])
+def tags():
+    global conn
+    conn = open_db()
+
+    if request.method == 'POST':
+        the_form = request.form
+        if 'to_delete' in the_form.keys():
+            tmp = the_form['to_delete'].split(":")
+            the_tag = {tmp[0]:tmp[1]}
+            utils.delete_tag(conn, the_tag)
+            conn.commit()
+        else:
+            the_tag = {the_form['type']: the_form['tag']}
+            utils.insert_tag_type(conn, the_tag, 'german')
+            utils.insert_tag(conn, the_tag)
+            conn.commit()
+    tags = utils.get_tags_from_db(conn)
+    return render_template("tags.html",tags=tags)
+
+
+@app.route('/tagwords', methods=['GET', 'POST'])
+def tag_words():
+    global conn
+    conn = open_db()
+    if request.method == 'POST':
+        the_form = request.form
+        if 'filter' in the_form.keys():
+            if the_form.get('case-sensitive'):
+                filter = f"'{the_form['filter']}'"
+            else:
+                filter = f"'(?i){the_form['filter']}'"
+            words = utils.get_words_from_db(conn,filter)
+            tags = utils.get_tags_from_db(conn)
+            print(words)
+            return render_template('tagwords.html', words=words, tags=tags)
+        else:
+            for word in the_form.items():
+                form_info = word[0].split('-')
+                word_id = form_info[0]
+                the_tag = {form_info[1]:form_info[2]}
+                utils.tag_word_by_id(conn, word_id, the_tag)
+            conn.commit()
+            return render_template('tagwords.html')
+    else:
+        return render_template('tagwords.html')
+
+
+if __name__ == '__main__':
+    conn = None
+    app.secret_key = '@xm@164'
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
+    app.run()
---
+++ utils.py
@@ -0,0 +1,284 @@
+import sqlite3
+import random
+
+
+def table_exists(cur, table_name):
+    # get the count of tables with the name
+    cnt = cur.execute(f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{table_name}' ''').fetchone()
+    ret_value = False if cnt[0] == 0 else True
+    return ret_value
+
+
+def initialize_database(conn):
+    cur = conn.cursor()
+    if table_exists(cur, "language") == False:
+        conn.execute('''CREATE TABLE language (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL,
+        UNIQUE (name) ON CONFLICT IGNORE);''')
+    if table_exists(cur, "word") == False:
+        conn.execute('''CREATE TABLE word(
+        id INTEGER PRIMARY KEY NOT NULL, 
+        from_lang_id INTEGER KEY NOT NULL, 
+        name VARCHAR NOT NULL,
+        to_lang_id INTEGER KEY NOT NULL, 
+        translation VARCHAR NOT NULL,
+        FOREIGN KEY (from_lang_id) REFERENCES language(id),
+        FOREIGN KEY (to_lang_id) REFERENCES language(id),                
+        UNIQUE (name, translation) ON CONFLICT IGNORE
+        );''')
+    if table_exists(cur, "tag_type") == False:
+        conn.execute('''CREATE TABLE tag_type (
+        id INTEGER PRIMARY KEY NOT NULL,
+        lang_id INTEGER KEY NOT NULL, 
+        name VARCHAR NOT NULL,        
+        FOREIGN KEY (lang_id) REFERENCES language(id),
+        UNIQUE (lang_id, name) ON CONFLICT IGNORE
+        );''')
+    if table_exists(cur, "tag") == False:
+        conn.execute('''CREATE TABLE tag (
+        id INTEGER PRIMARY KEY NOT NULL, 
+        tag_type_id INTEGER KEY NOT NULL, 
+        name VARCHAR NOT NULL,
+        FOREIGN KEY (tag_type_id) REFERENCES tag(id),
+        UNIQUE (name) ON CONFLICT IGNORE
+        );''')
+    if table_exists(cur, "tagged") == False:
+        conn.execute('''CREATE TABLE tagged (
+        id INTEGER PRIMARY KEY NOT NULL,         
+        word_id INTEGER KEY NOT NULL, 
+        tag_id INTEGER KEY NOT NULL,
+        FOREIGN KEY (word_id) REFERENCES word(id),
+        FOREIGN KEY (tag_id) REFERENCES tag(id)
+        );''')
+    conn.commit()
+
+def delete_table(conn, table):
+    sql = f'DELETE FROM {table};'
+    cur = conn.cursor()
+    cur.execute(sql)
+
+def delete_all_data(conn):
+    delete_table(conn, 'word')
+    delete_table(conn, 'tag_type')
+    delete_table(conn, 'tag')
+    delete_table(conn, 'tagged')
+    conn.commit()
+
+def add_demo_content(conn):
+    insert_languages(conn)
+    a_word = {"page":10, "word":"Hallo Welt", "translation":"Hello world"}
+    the_word = {'word':a_word['word'],
+                'translation':a_word['translation'],
+                'from':'german',
+                'to':'greek'}
+    insert_word(conn, the_word)
+
+    the_tag = {'page': a_word['page']}
+    tag_word(conn, the_word, the_tag)
+    the_tag = {'count': random.randint(1000,2000)}
+    tag_word(conn, the_word, the_tag)
+
+    a_word = {"page":10, "word":"Hallo Welt", "translation":"Hello over the world"}
+    the_word = {'word':a_word['word'],
+                'translation':a_word['translation'],
+                'from':'german',
+                'to':'greek'}
+    insert_word(conn, the_word)
+
+    the_tag = {'page': a_word['page']}
+    tag_word(conn, the_word, the_tag)
+    the_tag = {'count': random.randint(1000,2000)}
+    tag_word(conn, the_word, the_tag)
+
+    a_word = {"page": 10, "word": "Ja", "translation": "Yes"}
+    the_word = {'word':a_word['word'],
+                'translation':a_word['translation'],
+                'from':'german',
+                'to':'greek'}
+    insert_word(conn, the_word)
+
+    the_tag = {'page': a_word['page']}
+    tag_word(conn, the_word, the_tag)
+    the_tag = {'count': random.randint(1000, 2000)}
+    tag_word(conn, the_word, the_tag)
+
+    a_word = {"page": 10, "word": "genug", "translation": "Yes"}
+    the_word = {'word':a_word['word'],
+                'translation':a_word['translation'],
+                'from':'german',
+                'to':'greek'}
+
+    insert_word(conn, the_word)
+    the_tag = {'page': a_word['page']}
+    tag_word(conn, the_word, the_tag)
+    the_tag = {'count': random.randint(1000, 2000)}
+    tag_word(conn, the_word, the_tag)
+    conn.commit()
+
+
+def insert_languages(conn):
+    langs = ['german','greek']
+    for lang in langs:
+        sql = '''INSERT INTO language(name) VALUES(?)'''
+        conn.execute(sql,(lang,))
+
+
+def get_id(conn, table, criteria):
+    #find the id of the name
+    cur = conn.cursor()
+    for key, value in criteria.items():
+        key = key
+        value = value
+    sql = f"SELECT id FROM {table} WHERE {key}=?"
+    ids = cur.execute(sql,(value,)).fetchall()
+    if len(ids) == 1:
+        ids = ids[0][0]
+    else:
+        ids = 0
+    return ids
+
+
+def tag_word_by_dict(conn, a_word, a_tag):
+    cur = conn.cursor()
+    #get word_id in combination with translation_id
+    sql = "SELECT word.id FROM word WHERE word.name=? AND word.translation=?"
+    word_ids = cur.execute(sql,(a_word['word'], a_word['translation'],)).fetchall()
+
+    #check if tag_type exists
+    the_key = list(a_tag.keys())[0]
+    tag_type_id = get_id(conn, 'tag_type',{'name':the_key})
+    # tag_type was not found in the db
+    if tag_type_id == 0:
+        insert_tag_type(conn, a_tag, a_word['from'])
+
+    #check if tag_id exists
+    tag_id = get_id(conn, 'tag', {'name': a_tag[the_key]})
+    if tag_id == 0:
+        insert_tag(conn, a_tag)
+        tag_id = get_id(conn, 'tag', {'name': a_tag[the_key]})
+
+    #finally tag word
+    for word_id in word_ids:
+        sql = "INSERT INTO tagged(word_id, tag_id) VALUES(?,?)"
+        cur.execute(sql, (word_id[0], tag_id, ))
+
+
+def tag_word_by_id(conn, word_id, a_tag):
+    cur = conn.cursor()
+
+    the_key = list(a_tag.keys())[0]
+    tag_type_id = get_id(conn, 'tag_type',{'name':the_key})
+    tag_id = get_id(conn, 'tag', {'name': a_tag[the_key]})
+
+    #finally tag word
+    sql = "INSERT INTO tagged(word_id, tag_id) VALUES(?,?)"
+    cur.execute(sql, (word_id, tag_id, ))
+
+
+def delete_tag(conn, a_tag):
+    cur = conn.cursor()
+    for key, value in a_tag.items():
+        key = key
+        value = value
+    #Get tag type id
+    sql = f"SELECT id FROM tag_type WHERE name=?"
+    ids = cur.execute(sql, (key,)).fetchall()
+    if len(ids) == 1:
+        tag_type_id = ids[0][0]
+    else:
+        tag_type_id = 0
+    sql = "DELETE FROM tag WHERE tag.name = ? AND tag.tag_type_id=?"
+    ids = cur.execute(sql,(value, tag_type_id,)).fetchall()
+    #Check if type tag has been orfan and delete it if so
+    sql = "SELECT COUNT(id) FROM tag WHERE tag.tag_type_id = ?"
+    ids = cur.execute(sql, (tag_type_id,)).fetchall()
+    count = -1
+    if len(ids) == 1:
+        count = ids[0][0]
+    if count == 0:
+        sql = "DELETE FROM tag_type WHERE id = ?"
+        cur.execute(sql, (tag_type_id,))
+
+
+
+def insert_tag(conn, a_tag):
+    cur = conn.cursor()
+    sql = "INSERT INTO tag(name, tag_type_id) VALUES(?,?)"
+    the_key = list(a_tag.keys())[0]
+    tag_type_id = get_id(conn, 'tag_type', {'name':the_key})
+    cur.execute(sql, (a_tag[the_key], tag_type_id,))
+
+
+def insert_tag_type(conn, a_tag, lang):
+    cur = conn.cursor()
+    sql = "INSERT INTO tag_type(name, lang_id) VALUES(?,?)"
+    the_key = list(a_tag.keys())[0]
+    lang_id = get_id(conn,'language',{'name':lang})
+    cur.execute(sql, (the_key, lang_id,))
+
+
+def insert_word(conn, a_word):
+    cur = conn.cursor()
+
+    #find the id of the languages
+    to_lang_id = get_id(conn,'language', {'name': a_word['to']})
+    from_lang_id = get_id(conn,'language', {'name': a_word['from']})
+
+    #insert word to the word table
+    sql = "INSERT INTO word(name, from_lang_id, translation, to_lang_id) VALUES(?,?,?,?)"
+    cur.execute(sql, (a_word['word'], from_lang_id, a_word['translation'], to_lang_id))
+    return cur.lastrowid
+
+def get_word_dict(conn, the_id):
+    cur = conn.cursor()
+    sql = "SELECT * FROM word WHERE id=?"
+    cur.execute(sql, (the_id,))
+def get_words_from_db(conn, filter = ""):
+    cur = conn.cursor()
+    if filter == "":
+        sql = "SELECT word.id, word.name, word.translation, " \
+              "GROUP_CONCAT(tag.id), GROUP_CONCAT(tag.name), GROUP_CONCAT(tag_type.name) " \
+              "FROM word, tagged, tag, tag_type " \
+              "WHERE word.id=tagged.word_id AND " \
+              "tagged.tag_id=tag.id AND " \
+              "tag_type.id=tag.tag_type_id " \
+              "GROUP BY word.id"
+    else:
+        sql = f"SELECT word.id, word.name, word.translation, " \
+              f"GROUP_CONCAT(tag.id), GROUP_CONCAT(tag.name), GROUP_CONCAT(tag_type.name) " \
+              f"FROM word, tagged, tag, tag_type " \
+              f"WHERE word.id=tagged.word_id AND " \
+              f"tagged.tag_id=tag.id AND " \
+              f"tag_type.id=tag.tag_type_id AND " \
+              f"word.name REGEXP {filter} " \
+              f"GROUP BY word.id"
+    words = cur.execute(sql,).fetchall()
+    ret_value=[]
+    the_tags = {'tagid':[], 'keys':[], 'vals':[] }
+    word_row = {'id': [], 'word': [], 'translation': [], 'tags': []}
+    for word in words:
+        word_row['id']=word[0]
+        word_row['word'] = word[1]
+        word_row['translation'] = word[2]
+
+        tag_ids = word[3].split(',')
+        tag_vals = word[4].split(',')
+        tag_keys = word[5].split(',')
+        for i in range(0,len(tag_ids)):
+            the_tags['tagid'].append(tag_ids[i])
+            the_tags['keys'].append(tag_keys[i])
+            the_tags['vals'].append(tag_vals[i])
+        word_row['tags'] = the_tags
+        ret_value.append(word_row)
+    return ret_value
+
+def get_tags_from_db(conn):
+    cur = conn.cursor()
+    sql = "SELECT tag_type.name, GROUP_CONCAT(tag.name) " \
+          "FROM tag, tag_type " \
+          "WHERE tag.tag_type_id=tag_type.id " \
+          "GROUP BY tag_type.name"
+    tags = cur.execute(sql,).fetchall()
+    ret_value = [{tag[0]:tag[1].split(',')} for tag in tags]
+    return ret_value
